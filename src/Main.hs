ee78ee7a7b4b987d7cfac1a73d93d1561f95e41c
Frerich Raabe <raabe@froglogic.com>
2014-08-15 00:37:14 +0200

    Initial version of the Brainfuck interpreter
    First time I used a mutable vector in Haskell, seemed to work alright.
    The interpreter is feature-complete but I didn't profile it yet.

---
+++ src/Main.hs
@@ -0,0 +1,114 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+
+import qualified Data.Vector as V
+import qualified Data.Vector.Mutable as MV
+import Control.Exception (throwIO, Exception, catch)
+import Control.Monad (unless)
+import Data.Functor ((<$>))
+import Data.Char (ord, chr)
+import Data.Typeable (Typeable)
+import System.Exit (exitFailure)
+import System.Environment (getArgs)
+
+data Machine = Machine (V.Vector Char) Int (MV.IOVector Int) Int
+
+data InterpreterException = AtStartOfMemory
+                          | AtEndOfMemory
+                          | NoLoopStart
+                          | NoLoopEnd
+    deriving (Show, Typeable)
+
+instance Exception InterpreterException
+
+writeCell :: Machine -> Int -> IO ()
+writeCell (Machine _ _ mem ptr) = MV.write mem ptr
+
+readCell :: Machine -> IO Int
+readCell (Machine _ _ mem ptr) = MV.read mem ptr
+
+editCell :: (Int -> Int) -> Machine -> IO Machine
+editCell f m = f <$> readCell m >>= writeCell m >> return m
+
+editCellIdx :: (Int -> Int) -> Machine -> Machine
+editCellIdx f (Machine code codeIdx mem memIdx) = Machine code codeIdx mem (f memIdx)
+
+updateCodeIdx :: (Int -> Int) -> Machine -> Machine
+updateCodeIdx f (Machine code codeIdx mem memIdx) = Machine code (f codeIdx) mem memIdx
+
+findMatchingBracket :: Char -> Char -> (Int -> Int) -> V.Vector Char -> Int -> Maybe Int
+findMatchingBracket closeCh openCh step v i
+    | i >= 0 && i < V.length v =
+        if v V.! i == closeCh
+            then Just i
+            else
+                if v V.! i == openCh
+                    then step <$> findMatchingBracket closeCh openCh step v (step i) >>=
+                                  findMatchingBracket closeCh openCh step v
+                    else findMatchingBracket closeCh openCh step v (step i)
+    | otherwise = Nothing
+
+findClosingBracket :: V.Vector Char -> Int -> Maybe Int
+findClosingBracket = findMatchingBracket ']' '[' (+1)
+
+findOpeningBracket :: V.Vector Char -> Int -> Maybe Int
+findOpeningBracket = findMatchingBracket '[' ']' (subtract 1)
+
+exec :: Char -> Machine -> IO Machine
+exec ch = handle ch . updateCodeIdx (+1)
+  where
+    handle '>' m@(Machine _ _ mem memIdx)
+        | memIdx < MV.length mem = return (editCellIdx (+1) m)
+        | otherwise              = throwIO AtEndOfMemory
+    handle '<' m@(Machine _ _ _ memIdx)
+        | memIdx > 0 = return (editCellIdx (subtract 1) m)
+        | otherwise  = throwIO AtStartOfMemory
+    handle '+' m = editCell (+1) m
+    handle '-' m = editCell (subtract 1) m
+    handle '.' m = chr <$> readCell m >>= putChar >> return m
+    handle ',' m = ord <$> getChar >>= writeCell m >> return m
+    handle '[' m@(Machine code codeIdx mem memIdx) = do
+        cell <- readCell m
+        if cell == 0
+            then exitLoop
+            else return m
+      where
+        exitLoop =
+            case findClosingBracket code codeIdx of
+                Just codeIdx' -> return (Machine code (codeIdx' + 1) mem memIdx)
+                Nothing -> throwIO NoLoopEnd
+    handle ']' m@(Machine code codeIdx mem memIdx) = do
+        cell <- readCell m
+        if cell == 0
+            then return m
+            else restartLoop
+      where
+        restartLoop =
+            case findOpeningBracket code (codeIdx - 2) of
+                Just codeIdx' -> return (Machine code (codeIdx' + 1) mem memIdx)
+                Nothing -> throwIO NoLoopStart
+    handle _   m = return m
+
+boot :: Int -> String -> IO Machine
+boot memSize program = do
+    mem <- MV.replicate memSize 0
+    return (Machine (V.fromList program) 0 mem 0)
+
+run :: Machine -> IO ()
+run m@(Machine code codePtr _ _) =
+    unless (codePtr >= V.length code) $
+        exec (code V.! codePtr) m >>= run
+
+main :: IO ()
+main = do
+    [fileName] <- getArgs
+    machine <- readFile fileName >>= boot 1024
+    catch (run machine) $ \e -> do
+        putStr "... -- ERROR! "
+        case e of
+            AtStartOfMemory -> putStr "Cannot move left: already at start of memory."
+            AtEndOfMemory -> putStr "Cannot move right: already at end of memory."
+            NoLoopStart -> putStr "Cannot restart loop: loop beginning not found."
+            NoLoopEnd -> putStr "Cannot exit loop: loop end not found."
+        exitFailure
+    putChar '\n'
+
