67a59489c2433f30f5ea28fe300832b6bbd2e818
Frerich Raabe <raabe@froglogic.com>
2014-08-21 02:12:56 +0200

    Rewrote interpreter to first compile source code to optimized form
    Instead of executing the source code verbatim, generate an intermediate
    representation with less (but more powerful) commands. The assumption
    was that this would improve execution times, but alas it seems that
    things like factorial.bf became about 25% slower.
    
    However, the code looks nicer to me so let's keep this in a branch and
    see whether we can optimize later.

--- src/Main.hs
+++ src/Main.hs
@@ -1,103 +1,110 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE TemplateHaskell #-}
 
-import qualified Data.Vector as V
 import Control.Exception (throwIO, Exception, catch)
-import Control.Monad (unless)
-import Data.Functor ((<$>))
+import Control.Monad (foldM, void)
 import Data.Char (ord, chr)
 import Data.Typeable (Typeable)
-import Data.Label (mkLabels, get, set, modify)
+import Data.Label (mkLabels, get, modify)
 import Data.List.Zipper
 import System.Exit (exitFailure)
 import System.Environment (getArgs)
+import System.IO (hSetBuffering, stdout, BufferMode(NoBuffering))
 
-data Machine = Machine
-    { _code :: V.Vector Char
-    , _codeIdx :: Int
-    , _memory :: Zipper Int
-    }
+data Instruction
+    = AdjustCell Int
+    | AdjustCellPtr Int
+    | PutChar
+    | GetChar
+    | Loop Program
+    deriving (Show)
 
-mkLabels [''Machine]
-
-data InterpreterException = AtStartOfMemory
-                          | AtEndOfMemory
-                          | NoLoopStart
-                          | NoLoopEnd
+data InterpreterException
+    = AtStartOfMemory
+    | AtEndOfMemory
     deriving (Show, Typeable)
 
 instance Exception InterpreterException
 
+type Program = [Instruction]
+
+data Machine = Machine { _memory :: Zipper Int }
+mkLabels [''Machine]
+
+compose :: [a -> a] -> a -> a
+compose = foldr (.) id
+
 adjust :: (a -> a) -> Zipper a -> Zipper a
 adjust f z = replace (f . cursor $ z) z
 
-findMatchingBracket :: Char -> Char -> (Int -> Int) -> V.Vector Char -> Int -> Maybe Int
-findMatchingBracket closeCh openCh step v i
-    | i >= 0 && i < V.length v =
-        if v V.! i == closeCh
-            then Just i
-            else
-                if v V.! i == openCh
-                    then step <$> findMatchingBracket closeCh openCh step v (step i) >>=
-                                  findMatchingBracket closeCh openCh step v
-                    else findMatchingBracket closeCh openCh step v (step i)
-    | otherwise = Nothing
-
-findClosingBracket :: V.Vector Char -> Int -> Maybe Int
-findClosingBracket v idx = findMatchingBracket ']' '[' (+1) v (idx + 1)
-
-findOpeningBracket :: V.Vector Char -> Int -> Maybe Int
-findOpeningBracket v idx = findMatchingBracket '[' ']' (subtract 1) v (idx - 1)
-
-exec :: Char -> Machine -> IO Machine
-exec ch = handle ch . modify codeIdx (+1)
+compile :: String -> Either String Program
+compile = go [] []
   where
-    handle '>' m = execMemIdxShift (not . endp . get memory) AtEndOfMemory right m
-    handle '<' m = execMemIdxShift (not . beginp . get memory) AtStartOfMemory left m
-    handle '+' m = return (modify memory (adjust (+1)) m)
-    handle '-' m = return (modify memory (adjust (subtract 1)) m)
-    handle '.' m = putChar (chr . cursor . get memory $ m) >> return m
-    handle ',' m = do
-        c <- getChar
-        return (modify memory (replace . ord $ c) m)
-    handle '[' m = execJump (== 0) findClosingBracket NoLoopEnd m
-    handle ']' m = execJump (/= 0) findOpeningBracket NoLoopStart m
-    handle _   m = return m
+    go :: [Instruction] -> [[Instruction]] -> String -> Either String Program
+    go p stack s@(x:xs)
+        | x `elem` "+-" = let (v, rest) = compileSpan '+' '-' s in go (AdjustCell v : p) stack rest
+        | x `elem` "><" = let (v, rest) = compileSpan '>' '<' s in go (AdjustCellPtr v : p) stack rest
+        | x == '.'  = go (PutChar : p) stack xs
+        | x == ','  = go (GetChar : p) stack xs
+        | x == '['  = go [] (p:stack) xs
+        | x == ']'  = case stack of
+                        (a:as) -> go (Loop (reverse p) : a) as xs
+                        _      -> Left "unexpected ']'"
+        | otherwise = go p stack xs
+    go p [] [] = Right (reverse p)
+    go _ _  [] = Left "unexpected EOI, ']' missing"
 
-    execMemIdxShift p errorType idxAdjustment m
-        | p m       = return (modify memory idxAdjustment m)
-        | otherwise = throwIO errorType
+    compileSpan :: Char -> Char -> String -> (Int, String)
+    compileSpan inc dec str = (sum (map (\x -> if x == inc then 1 else (-1)) as), bs)
+      where
+        (as, bs) = span (`elem` [inc, dec]) str
 
-    execJump jumpCond locator errorType m = 
-        if not . jumpCond . cursor . get memory $ m then return m else
-            case locator (get code m) (get codeIdx m - 1) of
-                Just pos -> return (set codeIdx (pos + 1) m)
-                Nothing  -> throwIO errorType
+distLeft :: Zipper a -> Int
+distLeft (Zip l _) = length l
 
-boot :: Int -> String -> Machine
-boot memSize program =
-    Machine (V.fromList program) 0 (fromList (replicate memSize 0))
+distRight :: Zipper a -> Int
+distRight (Zip _ r) = length r
 
-run :: Machine -> IO ()
-run m = unless (i >= V.length c) (exec (c V.! i) m >>= run)
+exec :: Machine -> Instruction -> IO Machine
+exec machine i = handle i machine
   where
-    c = get code m
-    i = get codeIdx m
+    handle (AdjustCellPtr v) m
+        | v > 0 = if distRight (get memory m) >= v
+                    then return (modify memory (compose (replicate v right)) m)
+                    else throwIO AtEndOfMemory
+        | v < 0 = if distLeft (get memory m) >= (-v)
+                    then return (modify memory (compose (replicate (-v) left)) m)
+                    else throwIO AtStartOfMemory
+        | otherwise = return m
+    handle (AdjustCell v) m = return (modify memory (adjust (+v)) m)
+    handle PutChar m = putChar (chr . cursor . get memory $ m) >> return m
+    handle GetChar m = getChar >>= \c -> return (modify memory (replace . ord $ c) m)
+    handle l@(Loop p) m =
+        if cursor (get memory m) /= 0
+            then run m p >>= handle l
+            else return m
+
+run :: Machine -> Program -> IO Machine
+run = foldM exec
+
+boot :: Int -> Program -> IO ()
+boot memSize program = do
+    hSetBuffering stdout NoBuffering
+    let m = Machine (fromList (replicate memSize 0))
+    catch (void (run m program)) $ \e -> do
+        putStr "... ERROR! "
+        case e of
+            AtStartOfMemory -> putStrLn "Cannot move left: already at start of memory."
+            AtEndOfMemory -> putStrLn "Cannot move right: already at end of memory."
+        exitFailure
+    return ()
 
 main :: IO ()
 main = do
     args <- getArgs
-    program <- case args of
-                    [] -> getContents
-                    (fn:_) -> readFile fn
-    let machine = boot 65536 program
-    catch (run machine) $ \e -> do
-        putStr "... -- ERROR! "
-        case e of
-            AtStartOfMemory -> putStr "Cannot move left: already at start of memory."
-            AtEndOfMemory -> putStr "Cannot move right: already at end of memory."
-            NoLoopStart -> putStr "Cannot restart loop: loop beginning not found."
-            NoLoopEnd -> putStr "Cannot exit loop: loop end not found."
-        exitFailure
-    putChar '\n'
-
+    code <- case args of
+            [] -> getContents
+            (fn:_) -> readFile fn
+    case compile code of
+        Left errorMsg -> putStr errorMsg >> exitFailure
+        Right program -> boot 65536 program
